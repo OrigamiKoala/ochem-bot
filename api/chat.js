716cd7db860903c4971d2a1b8ba5ea3a8a63f60b
Carl Liu <142947035+OrigamiKoala@users.noreply.github.com>
2026-06-24 10:36:44 -0700

    fix errors

--- api/chat.js
+++ api/chat.js
@@ -815,13 +815,7 @@ export default async function handler(req, res) {
             };
 
             if (cacheSystemText) {
-                bodyPayload.system_instruction = {
-                    parts: [
-                        {
-                            text: cacheSystemText
-                        }
-                    ]
-                };
+                bodyPayload.system_instruction = cacheSystemText;
             }
 
             if (stream) {
